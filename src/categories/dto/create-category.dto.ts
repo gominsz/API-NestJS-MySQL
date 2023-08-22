f64c0f6d7d2bd71623dcc0f80d48fe538a55083e
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-21 22:50:58 -0300

    add-class-validator

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
+
 export class CreateCategoryDto {
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
     name: string;
     //(?) Significa que ela náo precisa ser passada, mas que ela pode ser ou náo uma string
+
+    @IsOptional()
+    @IsString()
     descripiton?: string | null;
 }
+ 
