82dee98f0d86acb789b344e0ac1d2ff8fb721532
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-22 15:08:49 -0300

    debug-upload/s

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -7,7 +7,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, 'upload/');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + Math.random() + path.extname(file.originalname));
