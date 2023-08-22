75be4c12e3b864abf05573f0da3b26296ec614ec
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-21 22:45:17 -0300

    adding functions to main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exceptions-filters/exception-filter';
+import { CatchAllErrorsExceptionFilter } from './exceptions-filters/catch-all-errors.exception-filter';
+import { ValidationPipe } from '@nestjs/common';
+import { InvalidRelationExceptionFilter } from './exceptions-filters/invalid-relation.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const {httpAdapter} = app.get(HttpAdapterHost);
 
-  app.useGlobalFilters(new PrismaExceptionFilter());
+
+  app.useGlobalFilters(
+    new CatchAllErrorsExceptionFilter(httpAdapter),
+    new PrismaExceptionFilter(),
+    new InvalidRelationExceptionFilter(),
+    );
+
+    app.useGlobalPipes(
+      new ValidationPipe ({
+        errorHttpStatusCode: 422,
+        transform: true,
+      })
+    )
 
   await app.listen(3000);
 }
